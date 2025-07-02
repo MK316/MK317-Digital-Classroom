2f26a983d2205310acdadfb28b4976f5ab7e669e
MK <99416359+MK316@users.noreply.github.com>
2025-07-02 15:01:58 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
 st.markdown("""
+### About this platform  
+
 + **Digital Teaching & Learning Archive:** This web platform is designed to archive and share classroom materials by semester for English Education majors. It hosts a variety of applications created to support both class instruction and student learning, tailored to the learning process and individual learner levels.
 
 + **Beyond accessing materials, this platform also reflects a broader goal:** encouraging future English teachers to explore how basic Python coding and web-based tools like Streamlit can be used to design customized, learner-centered applications. Through hands-on experience, students gain digital literacy skills essential for innovative and effective teaching in today’s classrooms.
