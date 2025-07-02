4c4e1dda2e3f41fd3b9d4f3b7c5fc648ed4d5489
MK <99416359+MK316@users.noreply.github.com>
2025-07-02 14:41:49 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.markdown("""
 + **Digital Teaching & Learning Archive:** This web platform is designed to archive and share classroom materials by semester for English Education majors. It hosts a variety of applications created to support both class instruction and student learning, tailored to the learning process and individual learner levels.
 
-Beyond accessing materials, this platform also reflects a broader goal: encouraging future English teachers to explore how basic Python coding and web-based tools like Streamlit can be used to design customized, learner-centered applications. Through hands-on experience, students gain digital literacy skills essential for innovative and effective teaching in today’s classrooms.
++ **Beyond accessing materials, this platform also reflects a broader goal:** encouraging future English teachers to explore how basic Python coding and web-based tools like Streamlit can be used to design customized, learner-centered applications. Through hands-on experience, students gain digital literacy skills essential for innovative and effective teaching in today’s classrooms.
 """)
 
 # Custom CSS for the styled button
